701f52ae6bd28c3b36354d0069145722776b529e
Dave P <dave_prosee@hotmail.com>
2016-11-27 20:52:02 +0100

    prevent controller from disabling steppers on timeout
    For clarity deactivation time is set to 0

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -339,7 +339,7 @@
 // Default stepper release if idle. Set to 0 to deactivate.
 // Steppers will shut down DEFAULT_STEPPER_DEACTIVE_TIME seconds after the last move when DISABLE_INACTIVE_? is true.
 // Time can be set by M18 and M84.
-#define DEFAULT_STEPPER_DEACTIVE_TIME 120
+#define DEFAULT_STEPPER_DEACTIVE_TIME 0  //no deactivation, we don't want to loose position
 #define DISABLE_INACTIVE_X false
 #define DISABLE_INACTIVE_Y false
 #define DISABLE_INACTIVE_Z false  // set to false if the nozzle will fall down on your printed part when print has finished.
